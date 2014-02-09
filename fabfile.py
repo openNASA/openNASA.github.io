a8a4a02759f7730dfda08dbe605ad0f402e790c0
Sean Herron <seanherron@gmail.com>
2014-02-08 20:49:29 -0500

    Test

--- fabfile.py
+++ fabfile.py
@@ -42,10 +42,10 @@ def preview():
     local('pelican -s publishconf.py')
     
 def ghb():
-    build()
+    #build()
     commit = prompt("Enter your commit text:")
     local('git commit -a -m "%s"' % commit)
-    local('git checkout gh-pages')
+    local('git checkout -b gh-pages')
     local('rm -rf !(output)')
     local('mv output .')
 
